a3e65f128f3ec296e4583ca386535cbc56d334fd
LukeSanderson18 <cactuar18@hotmail.com>
2019-01-19 15:57:17 +0000

    E

---
+++ Assets/Arm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MonoBehaviour
+{
+    /*public bool touching;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8) //if touching wall
+        {
+            touching = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8) //if touching wall
+        {
+            touching = false;
+        }
+    }
+    */
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /*private Rigidbody2D rb;
+    public GameObject armL;
+    public GameObject armR;
+
+    private SpriteRenderer armLSprite;
+    private SpriteRenderer armRSprite;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        armLSprite = armL.GetComponent<SpriteRenderer>();
+        armRSprite = armR.GetComponent<SpriteRenderer>();
+
+        rb.gravityScale = 0;
+
+        armLSprite.color = Color.white;
+        armRSprite.color = Color.white;
+
+        
+    }
+    */
+}
