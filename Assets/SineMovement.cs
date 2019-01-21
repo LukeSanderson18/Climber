b70f9a05d6411633d93b1d2a34847a7d97330bf0
LukeSanderson18 <cactuar18@hotmail.com>
2019-01-21 22:56:56 +0000

    dabbed on it

--- Assets/SineMovement.cs
+++ Assets/SineMovement.cs
@@ -6,16 +6,28 @@ public class SineMovement : MonoBehaviour {
 
     public float speed = 1;
     public float distance = 1;
+
+    public bool horizontal;
     float startY;
+    float startX;
+    Vector3 mov;
 
     private void Start()
     {
         startY = transform.position.y;
+        startX = transform.position.x;
     }
 
     void Update()
     {
-        Vector3 mov = new Vector3(transform.position.x, startY + Mathf.Sin(speed * Time.time) * distance, transform.position.z);
+        if (horizontal)
+        {
+            mov = new Vector3(startX + Mathf.Sin(speed * Time.time) * distance, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            mov = new Vector3(transform.position.x, startY + Mathf.Sin(speed * Time.time) * distance, transform.position.z);
+        }
         transform.position = mov;
     }
 }
